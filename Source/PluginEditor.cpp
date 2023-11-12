3e46f68e7e16b819889f15704425eba20af22157
natecdr <nathan.candre@gmail.com>
2023-11-12 17:02:00 +0100

    Add type and slope choice sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,9 +15,18 @@ ParametricEQ2AudioProcessorEditor::ParametricEQ2AudioProcessorEditor(ParametricE
 	band1GainVerticalSliderAttachment(audioProcessor.apvts, "band1_gain", band1GainVerticalSlider),
 	band2GainVerticalSliderAttachment(audioProcessor.apvts, "band2_gain", band2GainVerticalSlider),
 	band3GainVerticalSliderAttachment(audioProcessor.apvts, "band3_gain", band3GainVerticalSlider),
+
 	band1FreqRotarySliderAttachment(audioProcessor.apvts, "band1_freq", band1FreqRotarySlider),
 	band2FreqRotarySliderAttachment(audioProcessor.apvts, "band2_freq", band2FreqRotarySlider),
-	band3FreqRotarySliderAttachment(audioProcessor.apvts, "band3_freq", band3FreqRotarySlider)
+	band3FreqRotarySliderAttachment(audioProcessor.apvts, "band3_freq", band3FreqRotarySlider),
+
+	band1SlopeChoiceSliderAttachment(audioProcessor.apvts, "band1_slope", band1SlopeChoiceSlider),
+	band2SlopeChoiceSliderAttachment(audioProcessor.apvts, "band2_slope", band2SlopeChoiceSlider),
+	band3SlopeChoiceSliderAttachment(audioProcessor.apvts, "band3_slope", band3SlopeChoiceSlider),
+
+	band1TypeChoiceSliderAttachment(audioProcessor.apvts, "band1_type", band1TypeChoiceSlider),
+	band2TypeChoiceSliderAttachment(audioProcessor.apvts, "band2_type", band2TypeChoiceSlider),
+	band3TypeChoiceSliderAttachment(audioProcessor.apvts, "band3_type", band3TypeChoiceSlider)
 {
 	// Make sure that before the constructor has finished, you've set the
 	// editor's size to whatever you need it to be.
@@ -52,6 +61,7 @@ void ParametricEQ2AudioProcessorEditor::resized()
 	auto paramsArea = bounds.removeFromRight(bounds.getWidth() * 0.33);
 
 	auto bottomParamsArea = paramsArea.removeFromBottom(paramsArea.getHeight() * 0.2);
+	auto topParamsArea = paramsArea.removeFromTop(paramsArea.getHeight() * 0.2);
 
 	band1GainVerticalSlider.setBounds(paramsArea.removeFromLeft(paramsArea.getWidth() * 0.33));
 	band2GainVerticalSlider.setBounds(paramsArea.removeFromLeft(paramsArea.getWidth() * 0.5));
@@ -67,6 +77,17 @@ void ParametricEQ2AudioProcessorEditor::resized()
 	band1BandWidthRotarySlider.setBounds(bottomParamsBandWidthArea.removeFromLeft(bottomParamsBandWidthArea.getWidth() * 0.33));
 	band2BandWidthRotarySlider.setBounds(bottomParamsBandWidthArea.removeFromLeft(bottomParamsBandWidthArea.getWidth() * 0.5));
 	band3BandWidthRotarySlider.setBounds(bottomParamsBandWidthArea.removeFromLeft(bottomParamsBandWidthArea.getWidth()));
+
+	auto topParamsTypeArea = topParamsArea.removeFromTop(topParamsArea.getHeight() * 0.5);
+	auto topParamsSlopeArea = topParamsArea;
+
+	band1TypeChoiceSlider.setBounds(topParamsTypeArea.removeFromLeft(topParamsTypeArea.getWidth() * 0.33));
+	band2TypeChoiceSlider.setBounds(topParamsTypeArea.removeFromLeft(topParamsTypeArea.getWidth() * 0.5));
+	band3TypeChoiceSlider.setBounds(topParamsTypeArea.removeFromLeft(topParamsTypeArea.getWidth()));
+
+	band1SlopeChoiceSlider.setBounds(topParamsSlopeArea.removeFromLeft(topParamsSlopeArea.getWidth() * 0.33));
+	band2SlopeChoiceSlider.setBounds(topParamsSlopeArea.removeFromLeft(topParamsSlopeArea.getWidth() * 0.5));
+	band3SlopeChoiceSlider.setBounds(topParamsSlopeArea.removeFromLeft(topParamsSlopeArea.getWidth()));
 }
 
 std::vector<juce::Component*> ParametricEQ2AudioProcessorEditor::getComponents()
@@ -83,6 +104,14 @@ std::vector<juce::Component*> ParametricEQ2AudioProcessorEditor::getComponents()
 
 		&band1BandWidthRotarySlider,
 		&band2BandWidthRotarySlider,
-		&band3BandWidthRotarySlider
+		&band3BandWidthRotarySlider,
+
+		&band1SlopeChoiceSlider,
+		&band2SlopeChoiceSlider,
+		&band3SlopeChoiceSlider,
+
+		&band1TypeChoiceSlider,
+		&band2TypeChoiceSlider,
+		&band3TypeChoiceSlider,
 	};
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -35,6 +35,18 @@ struct CustomRotarySlider : juce::Slider
 	}
 };
 
+struct CustomChoiceSlider : juce::Slider
+{
+	CustomChoiceSlider() :
+		juce::Slider(
+			juce::Slider::SliderStyle::RotaryVerticalDrag,
+			juce::Slider::TextEntryBoxPosition::NoTextBox
+		)
+	{
+
+	}
+};
+
 //==============================================================================
 /**
 */
@@ -58,12 +70,17 @@ private:
 	CustomRotarySlider band1FreqRotarySlider, band2FreqRotarySlider, band3FreqRotarySlider;
 	CustomRotarySlider band1BandWidthRotarySlider, band2BandWidthRotarySlider, band3BandWidthRotarySlider;
 
+	CustomChoiceSlider band1SlopeChoiceSlider, band2SlopeChoiceSlider, band3SlopeChoiceSlider;
+	CustomChoiceSlider band1TypeChoiceSlider, band2TypeChoiceSlider, band3TypeChoiceSlider;
+
 	using APVTS = juce::AudioProcessorValueTreeState;
 	using Attachment = APVTS::SliderAttachment;
 
 	Attachment band1GainVerticalSliderAttachment, band2GainVerticalSliderAttachment, band3GainVerticalSliderAttachment;
 	Attachment band1FreqRotarySliderAttachment, band2FreqRotarySliderAttachment, band3FreqRotarySliderAttachment;
-	//Attachment band1BandWidthRotarySliderAttachment, band2BandWidthRotarySliderAttachment, band3BandWidthRotarySliderAttachment;
+
+	Attachment band1SlopeChoiceSliderAttachment, band2SlopeChoiceSliderAttachment, band3SlopeChoiceSliderAttachment;
+	Attachment band1TypeChoiceSliderAttachment, band2TypeChoiceSliderAttachment, band3TypeChoiceSliderAttachment;
 
 	std::vector<juce::Component*> getComponents();
 
